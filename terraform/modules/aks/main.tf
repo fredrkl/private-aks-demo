337ab4b47e2953f9a874e11f200f82734d729b42
Fredrik Klingenberg <fredrkl@gmail.com>
2023-10-29 09:44:37 +0100

    feat: terraform configuration in modules
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -7,3 +7,13 @@ resource "azurerm_virtual_network" "example" {
   location            = var.resource_group.location
   resource_group_name = var.resource_group.name
 }
+
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.77.0"
+    }
+  }
+}
