984f2acf2f7529d669396e0df74e7b039699d7e1
Fredrik Klingenberg <fredrkl@gmail.com>
2023-10-31 21:27:39 +0100

    feat: disable log analytics and policy
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -19,6 +19,7 @@ resource "azurerm_kubernetes_cluster" "example" {
     type = "SystemAssigned"
   }
 
+  azure_policy_enabled              = false
   role_based_access_control_enabled = true
 
   network_profile {
