a9a8958e68810c931934809cc74a49e4007ee39a
Fredrik Klingenberg <fredrkl@gmail.com>
2023-11-05 10:38:53 +0100

    feat: testing out with bastion
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -55,3 +55,27 @@ resource "azurerm_linux_virtual_machine" "example" {
     version   = "latest"
   }
 }
+
+#resource "azurerm_windows_virtual_machine" "example" {
+#  name                = "example-vm"
+#  resource_group_name = var.resource_group.name
+#  location            = var.resource_group.location
+#  size                = "Standard_DS1_v2"
+#  admin_username      = "adminuser"
+#  admin_password      = var.jumphost_secret
+#  network_interface_ids = [
+#    azurerm_network_interface.example.id,
+#  ]
+#
+#  os_disk {
+#    caching              = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
+#
+#  source_image_reference {
+#    publisher = "MicrosoftWindowsServer"
+#    offer     = "WindowsServer"
+#    sku       = "2019-Datacenter"
+#    version   = "latest"
+#  }
+#}
