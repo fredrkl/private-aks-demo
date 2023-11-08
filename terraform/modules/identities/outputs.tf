7f227e98e427049ef03064e506f81646a9431974
Fredrik Klingenberg <fredrkl@gmail.com>
2023-11-08 19:02:27 +0100

    feat: using client id from identity
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/modules/identities/outputs.tf
+++ terraform/modules/identities/outputs.tf
@@ -1,4 +1,4 @@
 output "aks_identity_id" {
   description = "The ID of the identity"
-  value       = azurerm_user_assigned_identity.example.id
+  value       = azurerm_user_assigned_identity.example.client_id
 }
