32bd19aee7995c8ae97bef716f2aed0d2da97727
Fredrik Klingenberg <fredrkl@gmail.com>
2023-11-12 15:22:06 +0100

    feat: rebuild aks
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -31,7 +31,7 @@ variable "bastion_admin_password" {
 variable "enable_aks" {
   description = "Feature flag to control the creation of the aks module resources"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "ssh_public_key" {
