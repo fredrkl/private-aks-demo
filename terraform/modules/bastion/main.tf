bfcad926399b4f6be135a05a8203415e83f934f8
Fredrik Klingenberg <fredrkl@gmail.com>
2023-11-14 21:05:03 +0100

    feat: upgrade jumphost to 22.04 LTS Ubuntu
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
 
